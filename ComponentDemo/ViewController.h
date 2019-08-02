b7e1c6146490941628153bbf9a7ae72e28625a78
mingma <mingmaf@isoftstone.com>
2019-08-02 15:42:50 +0800

    Initial Commit

---
+++ ComponentDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ComponentDemo
+//
+//  Created by ma on 2019/8/2.
+//  Copyright © 2019 ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ComponentDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ComponentDemo
+//
+//  Created by ma on 2019/8/2.
+//  Copyright © 2019 ma. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
